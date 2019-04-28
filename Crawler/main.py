e1b822320c20e1b1d93efcac877ce267808e0dd0
CYC <yenchen0416@gmail.com>
2019-04-28 23:47:29 +0800

    crawler data

--- Crawler/main.py
+++ Crawler/main.py
@@ -1,66 +1,46 @@
 import os
-import requests
-import json
 import time
-import csv
+import json
+import datetime
+import pandas as pd
 
-from collections import namedtuple
+from src.catch import catch_stock_data, save_csv
+
 
 with open("./config.json") as f:
     config = json.load(f)
 
-stock_id  = '2303'
-year = 2019
-month = 4
+file_path = os.path.join(config["DirPath"]["BuckupRoot"], config["FilePath"]["twse_equities"])
+twse_equities = pd.read_csv(file_path, encoding='utf-8')
+file_path = os.path.join(config["DirPath"]["BuckupRoot"], config["FilePath"]["tpex_equities"])
+tpex_equities = pd.read_csv(file_path, encoding='utf-8')
 
-# ['日期', '成交股數', '成交金額', '開盤價', '最高價', '最低價', '收盤價', '漲跌價差', '成交筆數']
-DATATUPLE = namedtuple('Data', ['date', 'capacity', 'turnover', 'open',
-                                'high', 'low', 'close', 'change', 'transaction'])
+stock_id_list = twse_equities['code'].values.tolist()
+stock_start_list = twse_equities['start'].values.tolist()
 
-def catch(year, month, stock_id):
-    param = {"date": '{:d}{:02d}01'.format(year, month) , 'stockNo': stock_id}
-    
-    url_twse_base = "http://www.twse.com.tw/"
-    url_source = os.path.join(url_twse_base, "exchangeReport/STOCK_DAY")
-    res = requests.get(url_source, param)
-    s = json.loads(res.text)
-    
-    time.sleep(1)
-    if s["stat"] == "OK":
-        data = _convert(s)
-    else:
-        pass
+stock_id  = "2303"
+# stock_start = '1985/07/16'
+stock_start = '2000/07/16'
+start_year = int(stock_start.split("/")[0])
+start_month = int(stock_start.split("/")[1])
 
-    return data
+current_time = datetime.datetime.now()
+current_year = current_time.year
+current_month = current_time.month
 
-def _standard_data(data):
-    data[0] = data[0]
-    data[1] = int(data[1].replace(",", ""))
-    data[2] = int(data[2].replace(",", ""))
-    data[3] = None if data[3] == '--' else float(data[3].replace(',', ''))
-    data[4] = None if data[4] == '--' else float(data[4].replace(',', ''))
-    data[5] = None if data[5] == '--' else float(data[5].replace(',', ''))
-    data[6] = None if data[6] == '--' else float(data[6].replace(',', ''))
-    data[7] = float(0.0 if data[7].replace(',', '') == 'X0.00' else data[7].replace(',', ''))
-    data[8] = int(data[8].replace(',', ''))
-    return DATATUPLE(*data)
+# data = catch_stock_data(2000, month, stock_id)
 
-def _convert(data):
-    result = [_standard_data(d) for d in data["data"]]
-    return result
+df = []
+for year in range(start_year, current_year+1, 1):
+    month_start = 1
+    month_end = 13
+    if year == current_year:
+        month_end = current_month
+    elif year == start_year:
+        month_start = start_month
 
-def save_csv(data, stock_id):
-    file_path = os.path.join(
-        config["DirPath"]["BuckupRoot"], config["DirPath"]["TaiwanStockPrice"], stock_id + ".csv")
-    
-    outputFile = open(file_path, 'w', newline='')
-    outputWriter = csv.writer(outputFile)
-    head = DATATUPLE(*data[0]._fields)
-    outputWriter.writerow(head)
-    for d in data:
-        outputWriter.writerow(d)
+    for month in range(month_start, month_end+1, 1):
+        data = catch_stock_data(year, month, stock_id)
+        df.extend(data)
 
-    outputFile.close()
-
-data = catch(year, month, stock_id)
-save_csv(data, stock_id)
+save_csv(df, stock_id)
