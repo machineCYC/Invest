e0d8010f668eff3024b989ada77a8bc86dd83c60
CYC <yenchen0416@gmail.com>
2019-06-09 21:06:56 +0800

    add us index

--- Code/FinMining/Features/F_ma.py
+++ Code/FinMining/Features/F_ma.py
@@ -12,15 +12,23 @@ with open("./config.json") as f:
 
 target_file = ['0050.csv', '0056.csv', '^TWII.csv']
 file_dir = os.path.join(config["DirPath"]["BuckupRoot"], config["DirPath"]["TaiwanStockPrice"])
-target_file = [os.path.join(file_dir, f) for f in target_file]
+target_file_path = [os.path.join(file_dir, f) for f in target_file]
+
+target_file = ['^DJI.csv', '^GSPC.csv']
+file_dir = os.path.join(config["DirPath"]["BuckupRoot"], config["DirPath"]["USStockPrice"])
+target_file_path.extend([os.path.join(file_dir, f) for f in target_file])
+
 save_file_dir = '../../data/Features'
 
 def main():
     need_col = ['stock_id', 'date', 'close']
     smoothing = analysis.Smoothing()
-    for file_path in target_file:
-        data = pd.read_csv(file_path, encoding='utf-8')[need_col]
+    for file_path in target_file_path:
+        data = pd.read_csv(file_path, encoding='utf-8')
+        data.columns = map(str.lower, data.columns)
+        data = data[need_col]
         data['MA20'] = smoothing.moving_means(data['close'].values, days=20)
+        data['bias20'] = (data['close'].values - data['MA20'].values) / data['MA20'].values
         if not os.path.exists(save_file_dir):
             os.mkdir(save_file_dir)
         data.to_csv(os.path.join(save_file_dir, 'F_ma_' + os.path.basename(file_path)), index=False)
