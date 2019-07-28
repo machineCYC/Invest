00c62c8348919cab6e4a4230cdd54aab5032298c
CYC <yenchen0416@gmail.com>
2019-07-28 16:22:09 +0800

    [ADD] plot

--- Code/FinVis/Home/view/Index.py
+++ Code/FinVis/Home/view/Index.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from Home import plots
 
 def Index(request):
-    return render(request, 'Index.html')
+    sto_pri_df = plots.get_Api_Data(dataset='TaiwanStockPrice', stock_id='0056', date='2015-01-01')
+    # sto_pri_df = plots.get_Api_Data(dataset='TaiwanStockPrice', stock_id='0056', date='2015-01-01')
+
+    context = {}
+    context['plot'] = plots.vis_0056(sto_pri_df)
+    # context['plot'] = plots.vis_test()
+    return render(request, 'Index.html', context)
 
 def Test(request):
     return HttpResponse(u"歡迎光臨!")
