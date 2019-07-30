8ab0f043e5cf87cfe86895358cf12553c9214ed1
CYC <yenchen0416@gmail.com>
2019-07-31 00:06:50 +0800

    [ADD] kd value

--- Code/FinVis/Home/plot/Index.py
+++ Code/FinVis/Home/plot/Index.py
@@ -43,12 +43,11 @@ def vis_test():
         layout=layout
     )
 
-    # py.iplot(fig, filename='using-tickformat-attribute-date')
     plot_div = plot(fig, output_type='div', include_plotlyjs=False, config=CONFIG)
 
     return plot_div
 
-def vis_0056(df):
+def vis_Twii(df):
     if len(df) == 0:
         return '暫無資料'
 
@@ -92,22 +91,30 @@ def vis_0056(df):
         "name": "120MA",
     }
     trace5 = {
-        'x': df['date'],
-        'y': (df['Trading_Volume'] / 1000).astype(int),
-        'marker': {'color': '#040273'},
-        "type": "bar",
-        "name": "vol",
+        "x": df['date'],
+        "y": df['K9'],
+        "line": {"width": 1},
+        "marker": {"color": '#3a68e8'},
+        "mode": "lines",
+        "name": "K9",
         'yaxis': "y2",
     }
-    data = [trace1, trace2, trace3, trace5]
+    trace6 = {
+        "x": df['date'],
+        "y": df['D9'],
+        "line": {"width": 1},
+        "marker": {"color": '#ed8e28'},
+        "mode": "lines",
+        "name": "D9",
+        'yaxis': "y2",
+    }
+    data = [trace1, trace2, trace3, trace4, trace5, trace6]
 
     layout = {
-        'title':'ETF(0056) 指數',
+        'title':'TWII 指數',
         'height': 800,
         'xaxis': {
             "rangeselector": {
-                "x": 0,
-                "y": 0.9,
                 "bgcolor": "rgba(150, 200, 250, 0.4)",
                 "buttons": [
                     {
@@ -154,6 +161,133 @@ def vis_0056(df):
         },
         'yaxis2':{
             'domain': [0, 0.2]
+        },
+    }
+
+    fig = go.Figure(data=data, layout=layout)
+    plot_div = plot(fig, output_type='div', include_plotlyjs=False, config=CONFIG)
+
+    return plot_div
+
+def vis_0056(df, twii_df):
+    if len(df) == 0:
+        return '暫無資料'
+
+    df['20MA'] = df['close'].rolling(window=20).mean()
+    df['60MA'] = df['close'].rolling(window=60).mean()
+    df['120MA'] = df['close'].rolling(window=120).mean()
+
+    trace1 = {
+        "x": df['date'],
+        "close": df['close'],
+        "decreasing": {"line": {"color": "green"}},
+        "high": df['max'],
+        "increasing": {"line": {"color": "red"}},
+        "low": df['min'],
+        "name": "GS",
+        "open": df['open'],
+        "type": "candlestick",
+    }
+    trace2 = {
+        "x": df['date'],
+        "y": df['20MA'],
+        "line": {"width": 1},
+        "marker": {"color": "#E377C2"},
+        "mode": "lines",
+        "name": "20MA",
+    }
+    trace3 = {
+        "x": df['date'],
+        "y": df['60MA'],
+        "line": {"width": 1},
+        "marker": {"color": "#77e38b"},
+        "mode": "lines",
+        "name": "60MA",
+    }
+    trace4 = {
+        "x": df['date'],
+        "y": df['120MA'],
+        "line": {"width": 1},
+        "marker": {"color": "#e3b677"},
+        "mode": "lines",
+        "name": "120MA",
+    }
+    trace5 = {
+        'x': df['date'],
+        'y': (df['Trading_Volume'] / 1000).astype(int),
+        'marker': {'color': '#0000e1'},
+        "type": "bar",
+        "name": "vol",
+        'yaxis': "y2",
+    }
+    trace6 = {
+        "x": twii_df['date'],
+        "close": twii_df['close'],
+        "decreasing": {"line": {"color": "green"}},
+        "high": twii_df['max'],
+        "increasing": {"line": {"color": "red"}},
+        "low": twii_df['min'],
+        "name": "GS",
+        "open": twii_df['open'],
+        "type": "candlestick",
+        'yaxis': "y3",
+    }
+    data = [trace1, trace2, trace3, trace4, trace5, trace6]
+
+    layout = {
+        'title':'ETF(0056) 指數',
+        'height': 1200,
+        'xaxis': {
+            "rangeselector": {
+                "bgcolor": "rgba(150, 200, 250, 0.4)",
+                "buttons": [
+                    {
+                        "count": 1,
+                        "label": "reset",
+                        "step": "all"
+                    },
+                    {
+                        "count": 1,
+                        "label": "1M",
+                        "step": "month",
+                        "stepmode": "backward"
+                    },
+                    {
+                        "count": 6,
+                        "label": "6M",
+                        "step": "month",
+                        "stepmode": "backward"
+                    },
+                    {
+                        "count": 1,
+                        "label": "1Y",
+                        "step": "year",
+                        "stepmode": "backward"
+                    },
+                    {
+                        "count": 3,
+                        "label": "3Y",
+                        "step": "year",
+                        "stepmode": "backward"
+                    },
+                    {
+                        "count": 5,
+                        "label": "5Y",
+                        "step": "year",
+                        "stepmode": "backward"
+                    },
+                ],
+                "font": {"size": 13}
+            },
+        },
+        'yaxis':{
+            'domain': [0.5, 1]
+        },
+        'yaxis2':{
+            'domain': [0.35, 0.47]
+        },
+        'yaxis3':{
+            'domain': [0, 0.32]
         },
     }
 
--- Code/FinVis/Home/view/Index.py
+++ Code/FinVis/Home/view/Index.py
@@ -1,13 +1,49 @@
+import numpy as np
+import pandas as pd
+
 from django.shortcuts import render
 from django.http import HttpResponse
 from Home import plots
+from django.views.generic import TemplateView
+
+def cal_RSV_Value(stock_price, days=9):
+    sp = stock_price
+
+    data = pd.DataFrame()
+    data['rolling_min'] = sp['min'].rolling(window = days).min()
+    data['rolling_max'] = sp['max'].rolling(window = days).max()
+    data['close'] = sp['close']
+    data['date'] = sp['date']
+    rsv = (data['close'] - data['rolling_min'])/(data['rolling_max']-data['rolling_min'])
+    rsv = round(rsv, 2)*100
+
+    return rsv
+
+def cal_KD_Value(df):
+    df['rsv'] = cal_RSV_Value(stock_price=df, days=9)
+    rsv = df['rsv'].values
+    rsv_na = rsv[np.isnan(rsv)]
+    rsv = rsv[~np.isnan(rsv)]
+
+    result = {'K_val':[50], 'D_val':[50]}
+    K_val_list = [50]
+    D_val_list = [50]
+    for i in range(1, len(rsv)):
+        K_value = (1/3) * rsv[i] + (2/3) * K_val_list[i-1]
+        K_val_list.append(K_value)
+        D_value = (2/3) * D_val_list[i-1] + (1/3) * K_val_list[i]
+        D_val_list.append(D_value)
+    return np.append(rsv_na, np.array(K_val_list)), np.append(rsv_na, np.array(D_val_list))
 
 def Index(request):
-    sto_pri_df = plots.get_Api_Data(dataset='TaiwanStockPrice', stock_id='0056', date='2015-01-01')
-    # sto_pri_df = plots.get_Api_Data(dataset='TaiwanStockPrice', stock_id='0056', date='2015-01-01')
+    date = '2018-01-01'
+    sto_pri_0056_df = plots.get_Api_Data(dataset='TaiwanStockPrice', stock_id='0056', date=date)
+    sto_pri_twii_df = plots.get_Api_Data(dataset='TaiwanStockPrice', stock_id='^TWII', date=date)
+    sto_pri_twii_df['K9'],  sto_pri_twii_df['D9'] = cal_KD_Value(sto_pri_twii_df)
 
     context = {}
-    context['plot'] = plots.vis_0056(sto_pri_df)
+    context['plot_0056'] = plots.vis_0056(sto_pri_0056_df, sto_pri_twii_df)
+    context['plot_TWII'] = plots.vis_Twii(sto_pri_twii_df)
     # context['plot'] = plots.vis_test()
     return render(request, 'Index.html', context)
 
