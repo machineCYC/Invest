be4ad8f0511a809e6674cc9856b8f8d24ea33d75
YenChen <yenchen0416@gmail.com>
2020-04-03 11:14:17 +0800

    [FIX] css path ivalid

--- Invest/Home/Home/urls.py
+++ Invest/Home/Home/urls.py
@@ -13,13 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from Asset.views import StockView
-from Asset.views import FundView
+from Asset.views import Fund
+from Asset.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Asset/Stock/', StockView),
-    path('Asset/Fund/', FundView),
-]
+    path('Asset/Fund/', Fund.as_view()),
+    path('', IndexView),
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
